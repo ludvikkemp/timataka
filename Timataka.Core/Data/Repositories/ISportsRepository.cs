f2f1167dcb2d2834c1960b7ea2fd5d66e2aaad26
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-01-28 17:55:54 +0000

    Add SportsRepository for demo purposes

---
+++ Timataka.Core/Data/Repositories/ISportsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Timataka.Core.Models.Entities;
+
+namespace Timataka.Core.Data.Repositories
+{
+    public interface ISportsRepository : IDisposable
+    {
+        void Insert(Sport entity);
+        Task InsertAsync(Sport entity);
+
+        IEnumerable<Sport> Get();
+
+        Sport GetById(int id);
+        Task<Sport> GetByIdAsync(int id);
+
+    }
+}
---
+++ Timataka.Core/Data/Repositories/SportsRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Timataka.Core.Models.Entities;
+
+namespace Timataka.Core.Data.Repositories
+{
+    
+    public class SportsRepository : ISportsRepository
+    {
+        private bool disposed = false;
+        private readonly ApplicationDbContext _context;
+
+        public SportsRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Sport> Get()
+        {
+            return _context.Sports.ToList();
+        }
+
+        public Sport GetById(int id)
+        {
+            return _context.Sports.SingleOrDefault(x => x.Id == id);
+        }
+
+        public Task<Sport> GetByIdAsync(int id)
+        {
+            return _context.Sports.SingleOrDefaultAsync(x => x.Id == id);
+        }
+
+        public void Insert(Sport entity)
+        {
+            _context.Sports.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public async Task InsertAsync(Sport entity)
+        {
+            _context.Sports.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+       
+       
+
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
