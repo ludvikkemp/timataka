e8c6807c8e2abd18d691a86ecfafddd491c382d5
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-01-28 16:34:05 +0000

    Add Sports Migration

--- Timataka.Core/Data/Migrations/20180128163317_Sport_Migration.Designer.cs
+++ Timataka.Core/Data/Migrations/20180128163317_Sport_Migration.Designer.cs
@@ -11,9 +11,10 @@ using Timataka.Core.Data;
 namespace Timataka.Core.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180128163317_Sport_Migration")]
+    partial class Sport_Migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Timataka.Core/Data/Migrations/20180128163317_Sport_Migration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Timataka.Core.Data.Migrations
+{
+    public partial class Sport_Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sports", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sports");
+        }
+    }
+}
