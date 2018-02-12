e85ac4eb40973bba21c9c108e03a049dcf24c6d5
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-02-12 16:14:25 +0000

    UnitTest Project Added and one test successful

---
+++ Timataka.Tests/SportsControllerTests.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Timataka.Core.Data;
+using Timataka.Core.Data.Repositories;
+using Timataka.Core.Models.Entities;
+using Timataka.Web.Controllers;
+using Xunit;
+
+namespace Timataka.Tests
+{
+    public class SportsControllerTests
+    {
+        [Fact]
+        public void GetSportsIndex()
+        {
+            // Arrange
+            var serviceMock = new Mock<ISportsRepository>();
+            serviceMock.Setup(x => x.Get()).Returns(() => new List<Sport>()
+            {
+                new Sport {Id = 1, Name = "Running"},
+                new Sport {Id = 2, Name = "Swimming"},
+                new Sport {Id = 3, Name = "Cycling"}
+            });
+            var controller = new SportsController(serviceMock.Object);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            ViewResult viewResult = Assert.IsType<ViewResult>(result);
+
+        }
+    }
+}
