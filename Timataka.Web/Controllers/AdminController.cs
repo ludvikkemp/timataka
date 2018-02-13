b27503f06d70fa6d6daace2716c5809cc5b6f276
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-02-13 23:47:20 +0000

    Playing around with UI layout (still working on it)

--- Timataka.Web/Controllers/AdminController.cs
+++ Timataka.Web/Controllers/AdminController.cs
@@ -1,18 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Timataka.Web.Controllers
 {
-    [Authorize(Roles = "Admin, Superadmin")]
+    
     public class AdminController : Controller
     {
+        
+        public AdminController()
+        {
+
+        }
+        [Authorize(Roles = "Admin, Superadmin")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            
+            return null;
         }
     }
 }
