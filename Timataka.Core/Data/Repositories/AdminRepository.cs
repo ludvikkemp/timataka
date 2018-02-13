a8988223a809f8aa2214f2945d784e1bbf9d1578
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-02-13 23:46:31 +0000

    AdminRepo and IAdminRepo created

---
+++ Timataka.Core/Data/Repositories/AdminRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Timataka.Core.Data.Repositories
+{
+    public class AdminRepository : IAdminRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AdminRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        
+    }
+}
