dd08b92ed71e29669cd5c760127669b70b4dfc28
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-02-15 22:56:34 +0000

    New admin routes
    Adding Dashboard and Users route in Admin controller.

--- Timataka.Web/Controllers/AdminController.cs
+++ Timataka.Web/Controllers/AdminController.cs
@@ -5,19 +5,34 @@ using System.Reflection.Emit;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Timataka.Core.Services;
 
 namespace Timataka.Web.Controllers
 {
     
     public class AdminController : Controller
     {
-        
-        public AdminController()
+        private readonly IAdminService _adminService;
+        public AdminController(IAdminService adminService)
         {
-
+            _adminService = adminService;
         }
         
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("Dashboard")]
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("Users")]
+        public IActionResult Users()
         {
             return View();
         }
