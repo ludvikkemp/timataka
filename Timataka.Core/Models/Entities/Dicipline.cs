9563cd9008d12650c2201d873cd7966260c72ea4
helgib10 <helgib10@ru.is>
2018-02-16 12:00:35 +0000

    Dicipline entity created

---
+++ Timataka.Core/Models/Entities/Dicipline.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Timataka.Core.Models.Entities
+{
+    class Dicipline
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [ForeignKey("Sport")]
+        public int SportId { get; set; }
+    }
+}
