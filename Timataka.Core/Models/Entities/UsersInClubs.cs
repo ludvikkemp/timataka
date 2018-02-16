bf773f3495c022bcafa9094d5743d1711baa212e
helgib10 <helgib10@ru.is>
2018-02-16 12:11:56 +0000

    UsersInClubs entity added

---
+++ Timataka.Core/Models/Entities/UsersInClubs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Timataka.Core.Models.Entities
+{
+    class UsersInClubs
+    {
+        public enum _Role {Member=0,Coach=1,Manager=2};
+
+        [ForeignKey("ApplicationUser")]
+        public int UserId { get; set; }
+        [ForeignKey("Sport")]
+        public int SportId { get; set; }
+        [ForeignKey("Club")]
+        public int ClubId { get; set; }
+        public _Role Role { get; set; }
+    }
+}
