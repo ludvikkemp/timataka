7eaa375a6109ae099097365f8ba10821e26e923f
ivarmarkusson <ivarmar15@ru.is>
2018-02-22 17:21:09 +0000

    Dicipline implemented

--- Timataka.Web/Controllers/DiciplineController.cs
+++ Timataka.Web/Controllers/DiciplineController.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Timataka.Core.Data;
+using Timataka.Core.Data.Repositories;
+using Timataka.Core.Models.Entities;
 
 namespace Timataka.Web.Controllers
 {
     public class DiciplineController : Controller
     {
+        private readonly IDiciplineRepository _repo;
+
+        public DiciplineController(IDiciplineRepository repo)
+        {
+            _repo = repo;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var diciplines = _repo.Get();
+            return View(diciplines);
         }
+
+
     }
 }
