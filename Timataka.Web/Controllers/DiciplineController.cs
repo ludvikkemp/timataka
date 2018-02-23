9eccf7bb44d886f1a041f72848e41ba3e6d2ccc9
ivarmarkusson <ivarmar15@ru.is>
2018-02-23 14:26:51 +0000

    Dicipline create/details/edit/delete implementation

--- Timataka.Web/Controllers/DiciplineController.cs
+++ Timataka.Web/Controllers/DiciplineController.cs
@@ -20,12 +20,75 @@ namespace Timataka.Web.Controllers
             _repo = repo;
         }
 
+        //GET: Dicipline
         public IActionResult Index()
         {
             var diciplines = _repo.Get();
             return View(diciplines);
         }
 
+        //GET: Dicipline/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
 
+        //POST: Dicipline/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Dicipline dicipline)
+        {
+            if (ModelState.IsValid)
+            {
+                await _repo.InsertAsync(dicipline);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dicipline);
+        }
+
+        //GET: Dicipline/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var dicipline = await _repo.GetByIdAsync(id);
+            if (dicipline == null)
+            {
+                return NotFound();
+            }
+
+            return View(dicipline);
+        }
+
+        //GET: Dicipline/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var dicipline = await _repo.GetByIdAsync(id);
+            if (dicipline == null)
+            {
+                return NotFound();
+            }
+            return View(dicipline);
+        }
+
+        //TODO
+        //POST: Dicipline/Edit/1
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("Id,Name")] Dicipline dicipline)
+        {
+            if (id != dicipline.Id)
+            {
+                return NotFound();
+            }
+
+            //TODO
+
+            return View();
+        }
+
+        //TODO
+        public IActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
