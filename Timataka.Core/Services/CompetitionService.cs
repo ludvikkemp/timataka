74658f782e24b5ee1f07e45b8a92b0b3d00dd0c1
helgib10 <helgib10@ru.is>
2018-03-05 16:05:02 +0000

    skeleton for CompetitonService

---
+++ Timataka.Core/Services/CompetitionService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Timataka.Core.Data.Repositories;
+using Timataka.Core.Models.Entities;
+
+namespace Timataka.Core.Services
+{
+    public class CompetitionService : ICompetitionService
+    {
+        private readonly ICompetitionRepository _repo;
+
+        public CompetitionService(ICompetitionRepository repo)
+        {
+            _repo = _repo;
+        }
+
+        public CompetitionService()
+        {
+            //For unit tests
+        }
+
+        public Task<Competition> Add(Competition c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Competition> Edit(Competition c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Competition> GetAllCompetitions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Competition> GetCompetitionById(int CompetitionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Remove(int CompetitionId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Timataka.Core/Services/ICompetitionService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Timataka.Core.Models.Entities;
+
+namespace Timataka.Core.Services
+{
+    public interface ICompetitionService
+    {
+        Task<Competition> Add(Competition c);
+        Task<Competition> Edit(Competition c);
+        Task<int> Remove(int CompetitionId);
+        IEnumerable<Competition> GetAllCompetitions();
+        Task<Competition> GetCompetitionById(int CompetitionId);
+    }
+}
