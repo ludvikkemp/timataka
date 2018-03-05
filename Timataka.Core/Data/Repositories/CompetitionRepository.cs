8a466e9b0e4b9d006bb330cc8dce7f304e6e4f1d
helgib10 <helgib10@ru.is>
2018-03-05 15:57:04 +0000

    competition repo template

---
+++ Timataka.Core/Data/Repositories/CompetitionRepository.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Timataka.Core.Models.Entities;
+
+namespace Timataka.Core.Data.Repositories
+{
+    public class CompetitionRepository : ICompetitionRepository
+    {
+        private bool _disposed = false;
+        private readonly ApplicationDbContext _context;
+
+        public CompetitionRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this._disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+
+            this._disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Create(Competition c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertAsync(Competition c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Competition> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Competition GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Competition> GetByIdAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Competition c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditAsync(Competition c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Competition c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(Competition c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Competition> GetCompetitionByNameAsync(string cName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Timataka.Core/Data/Repositories/ICompetitionRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Timataka.Core.Models.Entities;
+
+namespace Timataka.Core.Data.Repositories
+{
+    public interface ICompetitionRepository : IDisposable
+    {
+        void Create(Competition c);
+        Task InsertAsync(Competition c);
+
+        IEnumerable<Competition> Get();
+
+        Competition GetById(int Id);
+        Task<Competition> GetByIdAsync(int Id);
+
+        void Edit(Competition c);
+        Task EditAsync(Competition c);
+
+        void Remove(Competition c);
+        Task RemoveAsync(Competition c);
+
+        Task<Competition> GetCompetitionByNameAsync(string cName);
+    }
+}
