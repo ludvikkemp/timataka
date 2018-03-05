c18f34a5a38c424c3f3266ace867bd7a39852b85
helgib10 <helgib10@ru.is>
2018-03-05 16:11:41 +0000

    CompetitionController created

---
+++ Timataka.Web/Controllers/CompetitionController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Timataka.Core.Services;
+
+namespace Timataka.Web.Controllers
+{
+    public class CompetitionController : Controller
+    {
+        private readonly ICompetitionService _competitionService;
+
+        public CompetitionController(ICompetitionService competitionService)
+        {
+            _competitionService = competitionService;
+        }
+
+        // Get: Competitions
+        public IActionResult Index()
+        {
+            var competitions = _competitionService.GetAllCompetitions();
+            return View(competitions);
+        }
+    }
+}
