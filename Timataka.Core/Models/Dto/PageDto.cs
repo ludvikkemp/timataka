1e1befd8c65a6fc0da931eb8e0ffda74ad4ec666
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-03-10 17:45:46 +0000

    PageDto class created in Timatak.Core

---
+++ Timataka.Core/Models/Dto/PageDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Timataka.Core.Models.Dto
+{
+    public class PageDto<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int CurrentPage { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
