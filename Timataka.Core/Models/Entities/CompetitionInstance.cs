69d3b2dfdd351cbf73f78f6e92f5b7f986ca4e09
helgib10 <helgib10@ru.is>
2018-03-13 11:46:47 +0000

    Entity classes for event and competition instance

---
+++ Timataka.Core/Models/Entities/CompetitionInstance.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Timataka.Core.Models.Entities
+{
+    public enum Status
+    {
+        Pending = 0,
+        OpenForRegistration = 1,
+        Ongoing = 2,
+        Finished = 3,
+        Closed = 4
+    }
+    public class CompetitionInstance
+    {
+        public int Id { get; set; }
+        //Start of first event
+        public DateTime DateFrom { get; set; }
+        //End of last event
+        public DateTime DateTo { get; set; }
+        public String Location { get; set; }
+        public int? CountryId { get; set; }
+        [ForeignKey(nameof(CountryId))]
+        public virtual Country Country { get; set; }
+        public string CompetitionName { get; set; }
+        public Status Status { get; set; }
+
+    }
+}
