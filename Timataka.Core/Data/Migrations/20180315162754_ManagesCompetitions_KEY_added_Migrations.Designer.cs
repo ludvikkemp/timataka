04b621552b30c6c13a53b4572314d018fc6c372c
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-03-15 19:57:38 +0000

    migrations

--- Timataka.Core/Data/Migrations/20180315162754_ManagesCompetitions_KEY_added_Migrations.Designer.cs
+++ Timataka.Core/Data/Migrations/20180315162754_ManagesCompetitions_KEY_added_Migrations.Designer.cs
@@ -12,9 +12,10 @@ using Timataka.Core.Models.Entities;
 namespace Timataka.Core.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180315162754_ManagesCompetitions_KEY_added_Migrations")]
+    partial class ManagesCompetitions_KEY_added_Migrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -255,6 +256,8 @@ namespace Timataka.Core.Data.Migrations
 
                     b.Property<int>("CompetitionId");
 
+                    b.Property<string>("CompetitionName");
+
                     b.Property<int?>("CountryId");
 
                     b.Property<DateTime>("DateFrom");
@@ -264,8 +267,6 @@ namespace Timataka.Core.Data.Migrations
                     b.Property<bool>("Deleted");
 
                     b.Property<string>("Location");
-
-                    b.Property<string>("Name");
 
                     b.Property<int>("Status");
 
---
+++ Timataka.Core/Data/Migrations/20180315162754_ManagesCompetitions_KEY_added_Migrations.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Timataka.Core.Data.Migrations
+{
+    public partial class ManagesCompetitions_KEY_added_Migrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ManagesCompetitions_CompetitionId",
+                table: "ManagesCompetitions");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_ManagesCompetitions_CompetitionId_UserId",
+                table: "ManagesCompetitions",
+                columns: new[] { "CompetitionId", "UserId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_ManagesCompetitions_CompetitionId_UserId",
+                table: "ManagesCompetitions");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ManagesCompetitions_CompetitionId",
+                table: "ManagesCompetitions",
+                column: "CompetitionId");
+        }
+    }
+}
