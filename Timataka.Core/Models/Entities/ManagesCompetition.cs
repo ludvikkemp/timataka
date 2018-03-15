b6598c18036e7d385e05db4a5668d9742fa35123
helgib10 <helgib10@ru.is>
2018-03-15 14:00:08 +0000

    Entity class for ManagesCompetition

---
+++ Timataka.Core/Models/Entities/ManagesCompetition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Timataka.Core.Models.Entities
+{
+    public enum Role
+    {
+        Host = 0,
+        Official = 1,
+        Staff = 2
+    }
+
+    public class ManagesCompetition
+    {
+        public int UserId { get; set; }
+        public int CompetitionId { get; set; }
+        public Role Role { get; set; }
+    }
+}
