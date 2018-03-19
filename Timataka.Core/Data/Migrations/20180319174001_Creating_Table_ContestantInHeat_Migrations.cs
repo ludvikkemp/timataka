ea3b569d048316ba5638842ad84730399c1c91c0
ivarmarkusson <ivarmar15@ru.is>
2018-03-19 17:44:30 +0000

    ContestantInHeat migration and entity class

---
+++ Timataka.Core/Data/Migrations/20180319174001_Creating_Table_ContestantInHeat_Migrations.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Timataka.Core.Data.Migrations
+{
+    public partial class Creating_Table_ContestantInHeat_Migrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContestantsInHeats",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    HeatId = table.Column<int>(nullable: false),
+                    Bib = table.Column<int>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    Team = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContestantsInHeats", x => new { x.UserId, x.HeatId });
+                    table.UniqueConstraint("AK_ContestantsInHeats_HeatId_UserId", x => new { x.HeatId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_ContestantsInHeats_Heats_HeatId",
+                        column: x => x.HeatId,
+                        principalTable: "Heats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_ContestantsInHeats_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContestantsInHeats");
+        }
+    }
+}
---
+++ Timataka.Core/Models/Entities/ContestantInHeat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Timataka.Core.Models.Entities
+{
+    public class ContestantInHeat
+    {
+        [Key]
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        [Key]
+        public int HeatId { get; set; }
+        [ForeignKey(nameof(HeatId))]
+        public virtual Heat Heat { get; set; }
+
+        public int Bib { get; set; }
+        public string Team { get; set; }
+        public DateTime Modified { get; set; }
+    }
+}
