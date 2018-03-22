5b8eae3972d1e5628a4d675665b0a91e7b822ca5
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-03-22 15:36:53 +0000

    CourseDropDownListViewModel created

---
+++ Timataka.Core/Models/ViewModels/CourseViewModels/CourseViewModelDropDownList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Timataka.Core.Models.ViewModels.CourseViewModels
+{
+    public class CourseViewModelDropDownList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
