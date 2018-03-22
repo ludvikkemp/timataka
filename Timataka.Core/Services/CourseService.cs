b15e509c141ae116a2cef95f4d9e93e548c1d1a0
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-03-22 15:36:25 +0000

    Course and ICourse Services created

---
+++ Timataka.Core/Services/CourseService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Timataka.Core.Data;
+using Timataka.Core.Data.Repositories;
+using Timataka.Core.Models.ViewModels.CourseViewModels;
+
+namespace Timataka.Core.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly ICourseRepository _repo;
+
+        public CourseService(ICourseRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public IEnumerable<CourseViewModelDropDownList> GetCourseDropDown()
+        {
+            var courses = _repo.Get();
+            var dropDownList = courses.Select(course => new CourseViewModelDropDownList
+                {
+                    Id = course.Id,
+                    Name = course.Name
+                })
+                .ToList();
+            return dropDownList;
+        }
+    }
+}
