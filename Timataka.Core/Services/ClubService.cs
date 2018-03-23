10d08936bb939d13d86defb62c8afe982b80dfcf
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-03-23 15:10:59 +0000

    ClubService and ICLubService created

---
+++ Timataka.Core/Services/ClubService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Timataka.Core.Data.Repositories;
+
+namespace Timataka.Core.Services
+{
+    public class ClubService : IClubService
+    {
+        private readonly IClubRepository _repo;
+
+        public ClubService(IClubRepository repo)
+        {
+            _repo = repo;
+        }
+    }
+}
