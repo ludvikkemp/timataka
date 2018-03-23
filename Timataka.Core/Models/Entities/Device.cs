37fd2c4e06582fe80a66e4450b2709fb35061610
helgib10 <helgib10@ru.is>
2018-03-23 15:15:28 +0000

    Device and DevicesInEvent entity classes

---
+++ Timataka.Core/Models/Entities/Device.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Timataka.Core.Models.Entities
+{
+    public class Device
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Boolean Active { get; set; }
+    }
+}
---
+++ Timataka.Core/Models/Entities/DevicesInEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Timataka.Core.Models.Entities
+{
+    public class DevicesInEvent
+    {
+        public int DeviceId { get; set; }
+        [ForeignKey(nameof(DeviceId))]
+        public virtual Device Device { get; set; }
+        public int EventId { get; set; }
+        [ForeignKey(nameof(EventId))]
+        public virtual Event Event { get; set; }
+    }
+}
