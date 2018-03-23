16743e0e506a8950b78d37f163695a4998325723
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-03-23 17:44:24 +0000

    ViewModels for Clubs created

---
+++ Timataka.Core/Models/ViewModels/ClubViewModels/CreateClubViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Timataka.Core.Models.ViewModels.ClubViewModels
+{
+    public class CreateClubViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        public string NameAbbreviation { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Phone]
+        public string Phone { get; set; }
+        [Url]
+        public string Webpage { get; set; }
+    }
+}
---
+++ Timataka.Core/Models/ViewModels/ClubViewModels/EditClubViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Timataka.Core.Models.ViewModels.ClubViewModels
+{
+    public class EditClubViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string NameAbbreviation { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Phone]
+        public string Phone { get; set; }
+        [Url]
+        public string Webpage { get; set; }
+    }
+}
