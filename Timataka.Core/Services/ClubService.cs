081b2cdefc4af4e97db7d97c5e0d24f5eb1bd070
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-03-23 17:44:09 +0000

    ClubService and IClubService created

--- Timataka.Core/Services/ClubService.cs
+++ Timataka.Core/Services/ClubService.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Timataka.Core.Data.Repositories;
+using Timataka.Core.Models.Entities;
+using Timataka.Core.Models.ViewModels.ClubViewModels;
+using Timataka.Core.Models.ViewModels.CompetitionViewModels;
 
 namespace Timataka.Core.Services
 {
@@ -12,6 +16,54 @@ namespace Timataka.Core.Services
         public ClubService(IClubRepository repo)
         {
             _repo = repo;
+        }
+
+        public IEnumerable<Club> GetListOfCLubs()
+        {
+            var clubs =_repo.Get();
+            return clubs;
+        }
+
+        public async Task<bool> ClubExistsAsync(string modelName)
+        {
+            var result = await _repo.GetCompetitionByNameAsync(modelName);
+            if (result != null) return true;
+            return false;
+        }
+
+        public async Task<Club> AddAsync(CreateClubViewModel c)
+        {
+            var newClub = new Club
+            {
+                Name = c.Name,
+                Webpage = c.Webpage,
+                Email = c.Email,
+                Phone = c.Phone,
+                NameAbbreviation = c.NameAbbreviation
+            };
+            await _repo.InsertAsync(newClub);
+            return newClub;
+        }
+
+        public async Task<Club> EditClubAsync(EditClubViewModel m)
+        {
+            var c = await _repo.GetByIdAsync(m.Id);
+            await _repo.EditAsync(c);
+            return c;
+        }
+
+        public EditClubViewModel GetClubViewModelById(int id)
+        {
+            var c =_repo.GetById(id);
+            var model = new EditClubViewModel
+            {
+                Name = c.Name,
+                Webpage = c.Webpage,
+                NameAbbreviation = c.NameAbbreviation,
+                Email = c.Email,
+                Phone = c.Phone
+            };
+            return model;
         }
     }
 }
--- Timataka.Core/Services/IClubService.cs
+++ Timataka.Core/Services/IClubService.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Timataka.Core.Models.Entities;
+using Timataka.Core.Models.ViewModels.ClubViewModels;
 
 namespace Timataka.Core.Services
 {
     public interface IClubService
     {
+        IEnumerable<Club> GetListOfCLubs();
+        Task<bool> ClubExistsAsync(string modelName);
+        Task<Club> AddAsync(CreateClubViewModel c);
+        Task<Club> EditClubAsync(EditClubViewModel m);
+        EditClubViewModel GetClubViewModelById(int id);
     }
 }
