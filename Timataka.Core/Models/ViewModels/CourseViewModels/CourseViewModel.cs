eeb0b51cdc1c48fe896f5827fa4798525402f35a
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-03-25 23:40:31 +0000

    CourseViewModel Created

---
+++ Timataka.Core/Models/ViewModels/CourseViewModels/CourseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Timataka.Core.Models.ViewModels.CourseViewModels
+{
+    public class CourseViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Boolean Lap { get; set; }
+        public int Distance { get; set; }
+        public int DisciplineId { get; set; }
+        public string DisciplineName { get; set; }
+        public string ExternalCourseId { get; set; }
+    }
+}
