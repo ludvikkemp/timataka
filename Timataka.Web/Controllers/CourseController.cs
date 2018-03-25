3eeff837fc6eaa7d867bc3817fc303d3412cfe4e
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-03-25 23:42:35 +0000

    CourseController and CreateCourse created

---
+++ Timataka.Web/Controllers/CourseController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Timataka.Core.Models.ViewModels.CourseViewModels;
+using Timataka.Core.Services;
+
+namespace Timataka.Web.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CourseViewModel model)
+        {
+            if (ModelState.IsValid && model.Name != null)
+            {
+                var exists = await _courseService.CourseExistsAsync(model.Name);
+                if (!exists)
+                {
+                    await _courseService.AddAsync(model);
+                    return RedirectToAction("Courses", "Admin");
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult Edit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Details()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
