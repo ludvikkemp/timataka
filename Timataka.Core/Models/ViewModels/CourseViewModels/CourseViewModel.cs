a47e374dce2cd3cf0ed934cf0618c90940cd32de
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-03-26 16:13:53 +0000

    CourseViewModel edited, required added

--- Timataka.Core/Models/ViewModels/CourseViewModels/CourseViewModel.cs
+++ Timataka.Core/Models/ViewModels/CourseViewModels/CourseViewModel.cs
@@ -8,12 +8,15 @@ namespace Timataka.Core.Models.ViewModels.CourseViewModels
     public class CourseViewModel
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public Boolean Lap { get; set; }
         public int Distance { get; set; }
         [Display(Name = "Discipline")]
         public int DisciplineId { get; set; }
         public string DisciplineName { get; set; }
+        [Display(Name = "External Course Id")]
         public string ExternalCourseId { get; set; }
+        public bool Deleted { get; set; }
     }
 }
