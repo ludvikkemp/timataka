1ed3d60b42595a474b94696858f45b54a40e2bed
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-03-26 19:12:53 +0000

    UserViewModel and UserDto created

---
+++ Timataka.Core/Models/ViewModels/AdminViewModels/UserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Timataka.Core.Models.ViewModels.AdminViewModels
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Ssn { get; set; }
+        public string Phone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public bool Deleted { get; set; }
+        public string Role { get; set; }
+        public bool SuperAdmin { get; set; }
+    }
+}
