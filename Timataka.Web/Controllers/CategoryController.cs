29680a3e3c74a4cefe8e162233c87045ab1e2ff5
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-03-27 16:24:45 +0000

    Category Controller and Create

--- Timataka.Web/Controllers/CategoryController.cs
+++ Timataka.Web/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Timataka.Core.Models.ViewModels.CategoryViewModels;
 using Timataka.Core.Services;
 
 namespace Timataka.Web.Controllers
@@ -10,10 +11,41 @@ namespace Timataka.Web.Controllers
     public class CategoryController : Controller
     {
         private readonly ICategoryService _categoryService;
+        private readonly IEventService _eventService;
+        private readonly IAccountService _accountService;
 
-        public CategoryController(ICategoryService categoryService)
+        public CategoryController(
+            ICategoryService categoryService,
+            IEventService eventService,
+            IAccountService accountService)
         {
             _categoryService = categoryService;
+            _eventService = eventService;
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Events = _eventService.GetEventDropDownList();
+            ViewBag.Nations = _accountService.GetNationsListItems();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CategoryViewModel model)
+        {
+            if (ModelState.IsValid && model.Name != null)
+            {
+                var exists = await _categoryService.CategoryExistsAsync(model.Name);
+                if (!exists)
+                {
+                    await _categoryService.AddAsync(model);
+                    return RedirectToAction("Categories", "Admin");
+                }
+            }
+            return View(model);
         }
     }
 }
