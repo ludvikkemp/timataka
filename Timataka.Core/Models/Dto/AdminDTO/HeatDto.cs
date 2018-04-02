af9a13bfacc8a0aa02ed6a2a16ba11f9de1c1f36
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-04-02 16:38:33 +0000

    Competition breadcrumb and layout

--- Timataka.Core/Models/Dto/AdminDTO/HeatDto.cs
+++ Timataka.Core/Models/Dto/AdminDTO/HeatDto.cs
@@ -8,6 +8,9 @@ namespace Timataka.Core.Models.Dto.AdminDTO
 {
     public class HeatDto
     {
+        public Competition Competition { get; set; }
+        public CompetitionInstance CompetitionInstance { get; set; }
+        public Event Event { get; set; }
         public Heat Heat { get; set; }
         public IEnumerable<ContestantsInHeatViewModel> Contestants { get; set; }
     }
