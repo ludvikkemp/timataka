ea195dacb6415390b2b4d06917de5378783f0625
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-04-09 14:22:29 +0000

    Time entity created and migrated

---
+++ Timataka.Core/Data/Migrations/20180409141942_Adding_Times_Table_Migrations.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Timataka.Core.Data.Migrations
+{
+    public partial class Adding_Times_Table_Migrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Times",
+                columns: table => new
+                {
+                    HeatId = table.Column<int>(nullable: false),
+                    ChipCode = table.Column<string>(nullable: false),
+                    RawTime = table.Column<int>(nullable: false),
+                    TimeNumber = table.Column<int>(nullable: false),
+                    Type = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Times", x => new { x.HeatId, x.ChipCode });
+                    table.UniqueConstraint("AK_Times_ChipCode_HeatId", x => new { x.ChipCode, x.HeatId });
+                    table.ForeignKey(
+                        name: "FK_Times_Chips_ChipCode",
+                        column: x => x.ChipCode,
+                        principalTable: "Chips",
+                        principalColumn: "Code",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Times_Heats_HeatId",
+                        column: x => x.HeatId,
+                        principalTable: "Heats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Times");
+        }
+    }
+}
---
+++ Timataka.Core/Models/Entities/Time.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Timataka.Core.Models.Entities
+{
+    public enum TimeType    
+    {
+        Start = 0,
+        Split = 1,
+        Finish = 2
+    }
+    public class Time
+    {
+        [Key]
+        public string ChipCode { get; set; }
+        [ForeignKey(nameof(ChipCode))]
+        public virtual Chip Chip{ get; set; }
+
+        [Key]
+        public int HeatId { get; set; }
+        [ForeignKey(nameof(HeatId))]
+        public virtual Heat Heat { get; set; }
+
+        public int RawTime { get; set; }
+        public TimeType Type { get; set; }
+        public int TimeNumber { get; set; }
+    }
+}
