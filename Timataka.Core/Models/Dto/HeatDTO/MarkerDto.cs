81735e4feea4be4ed2ed09ae9bee58a57fc4e891
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-04-10 15:46:55 +0000

    MarkerDto In HeatController created

---
+++ Timataka.Core/Models/Dto/HeatDTO/MarkerDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Timataka.Core.Models.Entities;
+using Timataka.Core.Models.ViewModels.MarkerViewModels;
+
+namespace Timataka.Core.Models.Dto.HeatDTO
+{
+    public class MarkerDto
+    {
+        public IEnumerable<Marker> AssignedMarkers { get; set; }
+        public IEnumerable<Marker> MarkerList { get; set; }
+        public AssignMarkerToHeatViewModel AssingnMarkerToHeatViewModel { get; set; }
+
+        // Breadcrumbs
+        public string CompetitionName { get; set; }
+        public string CompetitionInstanceName { get; set; }
+        public string EventName { get; set; }
+        public int HeatNumber { get; set; }
+    }
+}
