48d1af8098738623d81d6eb0244eb277b0b2f40f
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-04-10 15:13:55 +0000

    CategoryDto created

---
+++ Timataka.Core/Models/Dto/AdminDTO/CategoryDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Timataka.Core.Models.ViewModels.CategoryViewModels;
+
+namespace Timataka.Core.Models.Dto.AdminDTO
+{
+    public class CategoryDto
+    {
+        public IEnumerable<CategoryViewModel> CategoryViewModels { get; set; }
+        
+        // BreadCrumbs
+        public string CompetitionName { get; set; }
+        public string CompetitonInstanceName { get; set; }
+        public string EventName { get; set; }
+    }
+}
