d81dea7a2f2ac285092930e3eaba41151bf87be1
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-04-15 14:13:15 +0000

    CreateChipViewModel created

---
+++ Timataka.Core/Models/ViewModels/ChipViewModels/CreateChipViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Timataka.Core.Models.ViewModels.ChipViewModels
+{
+    public class CreateChipViewModel
+    {
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        [Range(0, 10000, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Number { get; set; }
+        public Boolean Active { get; set; }
+    }
+}
