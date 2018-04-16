96d3da90cf866c47e750e1c5bb86c4922566d0a2
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-04-16 16:09:24 +0000

    ChipViewModels created

---
+++ Timataka.Core/Models/ViewModels/ChipViewModels/ScanChipViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Timataka.Core.Models.ViewModels.ChipViewModels
+{
+    public class ScanChipViewModel
+    {
+        [Required]
+        public string Code { get; set; }
+    }
+}
