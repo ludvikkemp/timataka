14e9f29f826d16d413e2e379a4761de9d694c950
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-04-17 15:13:14 +0000

    Edit Contestant View and Controller stuff

---
+++ Timataka.Core/Models/Dto/CompetitionInstanceDTO/EditContestantDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Timataka.Core.Models.Entities;
+using Timataka.Core.Models.ViewModels.ClubViewModels;
+using Timataka.Core.Models.ViewModels.ContestantViewModels;
+using Timataka.Core.Models.ViewModels.EventViewModels;
+
+namespace Timataka.Core.Models.Dto.CompetitionInstanceDTO
+{
+    public class EditContestantDto
+    {
+        public IEnumerable<ContestantInEventViewModel> Events { get; set; }
+
+        public string ContestantName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Nationality { get; set; }
+        public int? NationId { get; set; }
+        public string Phone { get; set; }
+    }
+
+}
