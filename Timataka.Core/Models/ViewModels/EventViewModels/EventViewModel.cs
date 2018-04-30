c8de9445f6ffd1c22789a4bb949daa5f9cb16237
ivarmarkusson <ivarmar15@ru.is>
2018-04-30 15:29:37 +0000

    Get catagories for event

--- Timataka.Core/Models/ViewModels/EventViewModels/EventViewModel.cs
+++ Timataka.Core/Models/ViewModels/EventViewModels/EventViewModel.cs
@@ -30,5 +30,6 @@ namespace Timataka.Core.Models.ViewModels.EventViewModels
         public bool ActiveChip { get; set; }
         public string DisciplineName { get; set; }
         public Boolean Deleted { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
     }
 }
