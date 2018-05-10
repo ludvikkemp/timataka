2136f8fbcee83a5afcacbd30d8756e2e52c05154
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-05-10 14:22:17 +0000

    changing Markers Table, and migrating

--- Timataka.Core/Data/Migrations/20180510141936_Changing_Markers_Table.Designer.cs
+++ Timataka.Core/Data/Migrations/20180510141936_Changing_Markers_Table.Designer.cs
@@ -12,9 +12,10 @@ using Timataka.Core.Models.Entities;
 namespace Timataka.Core.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180510141936_Changing_Markers_Table")]
+    partial class Changing_Markers_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Timataka.Core/Data/Migrations/20180510141936_Changing_Markers_Table.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Timataka.Core.Data.Migrations
+{
+    public partial class Changing_Markers_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "MarkerTime",
+                table: "Markers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MarkerTime",
+                table: "Markers");
+        }
+    }
+}
