d33fb67b77947d6b8e33c574aefd2f77b75fb117
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-05-10 14:35:16 +0000

    adding migrations og Markers table, TimeStamp instead of DateTime

---
+++ Timataka.Core/Data/Migrations/20180510143403_Changing_Markers_Table_TimeStamp_instead_of_DateTime.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Timataka.Core.Data.Migrations
+{
+    public partial class Changing_Markers_Table_TimeStamp_instead_of_DateTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
